acc4b60e3f2972e55f6a2503514966fe54c741db
Alan Jones <jones.alan21@gmail.com>
2018-06-16 13:08:04 -0400

    added tab animations to nav, organized JS better

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,37 +1,86 @@
-$(function(){
+const app = {};
+//variables that I'll need later
+app.aboutHeight = $('#anchor2').offset();
+app.windowWidth = $(window).width();
 
-    var windowWidth = $(window).width();
+//Determines whether the nav should load when the page loads based on window width.
 
-    if (windowWidth < 650) {
+app.initNavBar = () => {
+    if (app.windowWidth < 650) {
         $('.wow').removeClass('wow');
-    }
+    };
 
-    if ($(window).scrollTop() >= 300) {
+    if ($(window).scrollTop() >= app.aboutHeight.top) {
         $(".nav").addClass("notHidden");
-    }
+    };
+};
 
-    $(document).on('click', 'a[href^="#"]', function (event) {
-        event.preventDefault();
+//Smooth scroll functionality
 
+app.smoothScroll = () => {
+    $(document).on('click', 'a[href^="#"]', function (e) {
+        e.preventDefault();
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
     });
+};
 
+//WOW slide-in animations
+
+app.wowAnimations = () => {
     new WOW({
         offset: 100
     }).init();
+}
 
-    $(window).scroll(function () {
-        var scroll_top = $(this).scrollTop();
-        
-        if (scroll_top >= 300) {
-            $(".nav").addClass("notHidden");
-        } else {
-            $(".nav").removeClass("notHidden");
+//Checks window height and modifies nav accordingly
+app.checkWindowHeight = () => {
+    const $scrollTop = $(window).scrollTop();
+
+    for (let i = 1; i < 5; i++) {
+        const $anchorTop = $('#anchor' + i).offset().top;
+
+        const bottomIndex = i + 1;
+
+        const $anchorBottom = $('#anchor' + bottomIndex).offset().top;
+
+        if (i === 1) {
+            $('.nav__link--active').removeClass('nav__link--active')
         }
-    });
+        else if ($scrollTop > $anchorTop - 200 && $scrollTop < $anchorBottom - 200) {
+            $('.nav__link--active').removeClass('nav__link--active')
+            $('.anchor' + i).addClass('nav__link--active')
+        }
+        else if ($scrollTop > $('#anchor5').offset().top - 200) {
+            $('.nav__link--active').removeClass('nav__link--active')
+            $('.anchor5').addClass('nav__link--active')
 
+        };
+
+    };
+
+    if ($scrollTop >= app.aboutHeight.top - 200) {
+        $(".nav").addClass("notHidden");
+    }
+    else {
+        $(".nav").removeClass("notHidden");
+    };
+}
+
+//Tells the Nav to open and/or close depending on scroll position / allows tabs to darken on scroll action.
+
+app.scrollEvents = () => {
+    $(window).scroll(function () {
+        setTimeout(function () { 
+            app.checkWindowHeight();
+        }, 400);
+    });
+}
+
+//Hamburger Nav, baby
+
+app.mobileNav = () => {
     $('#nav-icon4').click(function () {
         $(this).toggleClass('open');
         $('.nav__ul, .nav').toggleClass('show');
@@ -40,15 +89,24 @@ $(function(){
     $('a').click(function () {
         $('.show').removeClass('show')
         $('#nav-icon4').removeClass('open');
-    })
+    });
 
     $(window).on('resize', function (event) {
         var windowWidth = $(window).width();
         if (windowWidth > 940) {
-            // $('.nav__ul, nav').width('150px')
             $('.show').removeClass('show')
             $('#nav-icon4').removeClass('open');
         }
     });
+}
 
-})
+app.init = () => {
+    app.initNavBar();
+    app.checkWindowHeight();
+    app.smoothScroll();
+    app.wowAnimations();
+    app.scrollEvents();
+    app.mobileNav();
+}
+
+$(app.init)
